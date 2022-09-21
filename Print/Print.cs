8a528f44d7f19aa13893de96ad7a9aa50f0b9545
Tejas M R <tejasmr@outlook.com>
2022-09-21 16:05:59 +0530

    Published library

---
+++ Print/Print.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+
+namespace Print
+{
+    public class Print
+    {
+        public static void print(params object[] objs)
+        {
+            Console.Write("[ ");
+            foreach (var o in objs)
+                InternalPrint(o);
+            Console.WriteLine("]");
+        }
+        private static void InternalPrint(object o)
+        {
+            IEnumerable enumerable = (o as IEnumerable);
+            if (enumerable != null && !(o is string))
+            {
+                Console.Write("[ ");
+                foreach (var item in enumerable)
+                {
+                    Console.Write(item + " ");
+                }
+                Console.Write("] ");
+            }
+            else
+            {
+                Console.Write(o + " ");
+            }
+        }
+    }
+}
